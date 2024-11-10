74f6eb84efa053e879c1dc084285a402f2cec882
slowsage <84777606+slowsage@users.noreply.github.com>
2024-11-10 17:22:16 -0500

    Migration of extensions from main anyrun repo

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,13 @@ on what each of these should be is found in their respective attribute macros.
 
 pub use anyrun_interface::{self, HandleResult, Match, PluginInfo};
 pub use anyrun_macros::{get_matches, handler, info, init};
+
+/*
+The macro to create a plugin, handles asynchronous execution of getting the matches and the boilerplate
+for integrating with `stable_abi`.
+
+# Arguments
+
+
+* `$type`: The type of the shared data to be provided to various functions.
+*/
